ef9ccbd77942ef173d5748012c3fd3b243d4aca0
christiandisra <109445267+christiandisraely@users.noreply.github.com>
2024-03-25 11:46:12 -0600

    primero
    primero

---
+++ Proyecto sitio web hospitalario/vista/login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+  <img class="wave"src="../img/wave.png" alt="">
+  <div class="contenedor">
+    <div class="img">
+        <img src="../img/undraw_medicine_b-1-ol" alt="">
+    </div>
+<div class="contenido_login">
+    <form action="">
+        <img src="../img/logo.png" alt="">
+        <h2>Hospital </h2>
+        <div class="imput-div contraseña">
+            <div class="i">
+                <i class="fas fa-user"></i>
+            </div>
+        <div class="div">
+            <h5>contraseña</h5>
+            <input type="text" name="user" class="input">
+        </div>
+        </div>
+        <div class="input-div pass">
+         <div class="i">
+            <i class=" fas fa-lock"></i>
+         </div>   
+         <div class="div">
+            <h5>contrase</h5>
+            <input type="password" name="pass" class="input">
+         </div>
+        </div>
+        <a href="">created warpiece</a>
+        <input type="submit" class="btn" value="iniciar sesion">
+    </form>
+
+</div> 
+
+  </div>
+    
+</body>
+</html>
